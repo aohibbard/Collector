d883fab44ca5f3655a97421881643701caac10d9
Andrew Hibbard <andrew.o.hibbard@gmail.com>
2020-05-07 13:08:25 -0400

    update environment.rb

--- config/environment.rb
+++ config/environment.rb
@@ -1,11 +1,17 @@
-ENV['SINATRA_ENV'] ||= "development"
+# ENV['SINATRA_ENV'] ||= "development"
 
 require 'bundler/setup'
 require 'sinatra/base'
 require 'rack-flash'
-Bundler.require(:default, ENV['SINATRA_ENV'])
+# Bundler.require(:default, ENV['SINATRA_ENV'])
+Bundler.require 
 
 configure :development do
+  ENV['SINATRA_ENV'] ||= "development"
+
+  require 'bundler/setup'
+  Bundler.require(:default, ENV['SINATRA_ENV'])
+
   ActiveRecord::Base.establish_connection(
   :adapter => "sqlite3",
   :database => "db/#{ENV['SINATRA_ENV']}.sqlite"
