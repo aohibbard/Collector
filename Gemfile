064a1a9221b2d51951943f263c30dc0f30803bf6
Andrew Hibbard <andrew.o.hibbard@gmail.com>
2020-05-07 09:26:24 -0400

    updates production db to postgresql

--- Gemfile
+++ Gemfile
@@ -5,7 +5,7 @@ gem 'activerecord', '~> 4.2', '>= 4.2.6', :require => 'active_record'
 gem 'sinatra-activerecord', :require => 'sinatra/activerecord'
 gem 'rake'
 gem 'require_all'
-gem 'sqlite3', '~> 1.3.6'
+# gem 'sqlite3', '~> 1.3.6'
 gem 'thin'
 gem 'shotgun'
 gem 'pry'
@@ -17,6 +17,15 @@ gem 'rack-flash3'
 gem 'json'
 gem 'hyperclient'
 # gem 'httparty'
+
+group :production do 
+  gem 'pg', '~> 0.18'
+  gem 'activerecord-postgresql-adapter'
+end 
+
+group :development, :test do 
+  gem 'sqlite3'
+end 
 
 group :test do
   gem 'rspec'
--- config/environment.rb
+++ config/environment.rb
@@ -5,10 +5,34 @@ require 'sinatra/base'
 require 'rack-flash'
 Bundler.require(:default, ENV['SINATRA_ENV'])
 
-ActiveRecord::Base.establish_connection(
+configure :development do
+  ActiveRecord::Base.establish_connection(
   :adapter => "sqlite3",
   :database => "db/#{ENV['SINATRA_ENV']}.sqlite"
-)
+  )
+end
+
+configure :production do
+  db = URI.parse(ENV['HEROKU_POSTGRESQL_COBALT_URL'] || 'postgres://localhost/mydb')
+
+  ActiveRecord::Base.establish_connection(
+    :adapter  => db.scheme == 'postgres' ? 'postgresql' : db.scheme,
+    :host     => db.host,
+    :username => db.user,
+    :password => db.password,
+    :database => db.path[1..-1],
+    :encoding => 'utf8'
+  )
+end
+
+# ActiveRecord::Base.establish_connection(
+#   :adapter => "sqlite3",
+#   :database => "db/#{ENV['SINATRA_ENV']}.sqlite"
+# )
+# ActiveRecord::Base.establish_connection(
+#   :adapter => "postgresql",
+#   :database => "db/#{ENV['SINATRA_ENV']}.sqlite"
+# )
 
 require './app/controllers/application_controller'
 require_all 'app'
